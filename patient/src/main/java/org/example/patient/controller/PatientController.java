c9014bf1b6c86cecc94e793ea43367a9ad8b0300
Astroabd <149486339+Astroabd@users.noreply.github.com>
2026-07-09 22:31:54 +0200

    Remove unnecessary blank line in PatientController

--- patient/src/main/java/org/example/patient/controller/PatientController.java
+++ patient/src/main/java/org/example/patient/controller/PatientController.java
@@ -11,7 +11,6 @@ import org.example.patient.service.PatientService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
 import java.util.List;
 
 
