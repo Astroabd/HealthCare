377d2229a3dd17d77277f7b2e3cb284fb588eb22
Astroabd <astroabed@gmail.com>
2026-07-21 15:12:05 +0200

    commit

--- assessment/src/main/java/org/example/assessment/client/PatientClient.java
+++ assessment/src/main/java/org/example/assessment/client/PatientClient.java
@@ -1,12 +1,13 @@
 package org.example.assessment.client;
 
 
-import org.example.patient.dto.PatientResponse;
+import org.example.assessment.dto.PatientResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient("PATIENT")
 public interface PatientClient {
-     ResponseEntity<PatientResponse> findPatientById(@PathVariable Long id);
+     @GetMapping("patients/{id}")
+     PatientResponse findPatientById(@PathVariable Long id);
 }
