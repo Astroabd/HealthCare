d50cc8253a98b75ae6b99e8521add8f901931413
Astroabd <astroabed@gmail.com>
2026-07-21 15:08:24 +0200

    commit

--- assessment/src/main/java/org/example/assessment/client/NotesClient.java
+++ assessment/src/main/java/org/example/assessment/client/NotesClient.java
@@ -1,8 +1,7 @@
 package org.example.assessment.client;
 
-import org.example.notes.dto.NotesResponse;
+import org.example.assessment.dto.NotesResponse;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
@@ -10,6 +9,6 @@ import java.util.List;
 
 @FeignClient("NOTES")
 public interface NotesClient {
-    @GetMapping("/patient/{patientId}")
-     ResponseEntity<List<NotesResponse>> findNotesByPatientId(@PathVariable Long patientId);
+    @GetMapping("notes/patient/{patientId}")
+     List<NotesResponse> findNotesByPatientId(@PathVariable Long patientId);
 }
