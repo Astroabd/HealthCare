da355a3b4e35a32873fe99a7ad321bf6401c9f4c
Astroabd <astroabed@gmail.com>
2026-07-21 15:10:06 +0200

    commit

--- assessment/src/main/java/org/example/assessment/AssessmentApplication.java
+++ assessment/src/main/java/org/example/assessment/AssessmentApplication.java
@@ -2,8 +2,10 @@ package org.example.assessment;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients
 public class AssessmentApplication {
 
     public static void main(String[] args) {
