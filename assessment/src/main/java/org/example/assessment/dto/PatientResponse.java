8a3dd0583a417b3ced6342a1ec11f27020e97abc
Astroabd <astroabed@gmail.com>
2026-07-21 15:11:55 +0200

    commit

--- assessment/src/main/java/org/example/assessment/dto/PatientResponse.java
+++ assessment/src/main/java/org/example/assessment/dto/PatientResponse.java
@@ -1,4 +1,14 @@
 package org.example.assessment.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+import org.example.assessment.enums.Sex;
+
+import java.time.LocalDate;
+
+@Getter
+@Setter
 public class PatientResponse {
+    private Sex sex;
+    private LocalDate dateOfBirth;
 }
