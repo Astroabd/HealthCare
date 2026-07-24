3ab14c5468f1164307905a247004ad067ff44b3b
Astroabd <astroabed@gmail.com>
2026-07-24 11:05:19 +0200

    commit

--- gateway/src/main/java/org/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/org/example/gateway/GatewayApplication.java
@@ -3,6 +3,7 @@ package org.example.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+
 @SpringBootApplication
 public class GatewayApplication {
 
